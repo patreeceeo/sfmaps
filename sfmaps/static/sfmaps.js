34591fb58eac0d24bad2663ce7b952ce674f79fb
Pzatrick <patrickcandoit@gmail.com>
2013-03-25 04:33:44 -0700

    added route filter

--- sfmaps/static/sfmaps.js
+++ sfmaps/static/sfmaps.js
@@ -1,6 +1,13 @@
 (function (d3, $, _) {
     'use strict';
 
+    var route_ui_state = d3.map({
+        F: true
+        , 17: true
+        , 90: true
+        , 91: true
+    });
+
     var MapView = function (el, options) {
         var getOption = function (name, fallback) {
             return options && options[name] || fallback;
@@ -14,6 +21,7 @@
         this.fill = getOption('fill', 'transparent');
         this.stroke = getOption('stroke', 'black');
         this.strokeWidth = getOption('strokeWidth', '1');
+        this.cssClass = getOption('cssClass', '');
     };
 
     _.extend(MapView.prototype, {
@@ -23,6 +31,7 @@
             var path_gen = d3.geo.path().projection(this.projection); 
             d3.select(this.el)
             .append('g')
+            .attr('class', this.cssClass)
             .selectAll('path')
             .data(this.data.features)
             .enter().append('path')
@@ -41,12 +50,8 @@
     $(document).ready(function() { 
 
         var projection = d3.geo.mercator();
-        projection.center([-122.5, 37.8]);
-        projection.scale(280000);
-
-        // _.each(["freeways", "neighborhoods"], function (name) {
-
-        // });
+        projection.center([-122.5, 37.788]);
+        projection.scale(220000);
 
         d3.json('static/data/neighborhoods.json', function (data) {
             var mv = new MapView('#js-app-container', {
@@ -106,26 +111,43 @@
                 };
             }
 
-            // var routes_defers = [];
-            _.each(routes, function (route) {
-                // if(route.tag == 'F') {
-                    $.get(url().routeConfig(route.tag), function (xml) {
+
+            var RouteView = function (route) {
+
+                var that = this;
+                this.route = route;
+
+            };
+
+            _.extend(RouteView.prototype, {
+                show: function (show) {
+                    var selection = $('.route-'+this.route.tag.replace(' ', '-'));
+                    if(show) {
+                        console.log('showing route',this.route);
+                        this.rendered && selection.show() || this.render();
+                        this.rendered = true;
+                    } else {
+                        selection.hide();
+                    } 
+                }
+                , render: function () {
+                    var that = this;
+                    $.get(url().routeConfig(this.route.tag), function (xml) {
                         var rc = $.xml2json(xml);
-                        // stops_map.set(route.tag, rc.route.stop);
                         var data = nextbus_path_to_geojson(rc.route.path);
-                        var mv = new MapView('#js-app-container', {
+                        that.map_view = new MapView('#js-app-container', {
                             data: data                        
                             , projection: projection
                             , stroke: '#'+rc.route.color
+                            , cssClass: 'route-'+that.route.tag.replace(' ','-')
                         });
-                        mv.render();
-                        // kind of cheating
-                        route.color = rc.route.color;
+                        that.map_view.render();
+                        that.color = rc.route.color;
 
                     });
 
                     var get_locs = function (cb) {
-                        $.get(url().vehicleLocations(route.tag, 0), function (xml) {
+                        $.get(url().vehicleLocations(that.route.tag, 0), function (xml) {
                             var locs = $.xml2json(xml);
                             cb(locs);
                         });
@@ -136,21 +158,20 @@
                     get_locs(function (locs) {
                         if(locs.vehicle) {
                             circles = circles.append('g')
-                                .selectAll('circle')
-                                .data(locs.vehicle);
+                            .attr('class', 'route-'+that.route.tag.replace(' ', '-'))
+                            .selectAll('circle')
+                            .data(locs.vehicle);
 
                             circles.enter()
-                                .append('circle')
-                                .attr('cx', function (stop) {
-                                    return get_stop_xy(stop)[0];
-                                })
-                                .attr('cy', function (stop) {
-                                    return get_stop_xy(stop)[1];
-                                })
-                                .attr('r', 5)
-                                .attr('fill', function () {
-                                    return '#'+route.color;
-                                });
+                            .append('circle')
+                            .attr('cx', function (stop) {
+                                return get_stop_xy(stop)[0];
+                            })
+                            .attr('cy', function (stop) {
+                                return get_stop_xy(stop)[1];
+                            })
+                            .attr('r', 5)
+                            .attr('fill', '#'+that.color);
                         }
                     });
 
@@ -159,22 +180,46 @@
                             circles = circles.data(locs.vehicle);
 
                             circles.transition()
-                                .attr('cx', function (stop) {
-                                    return get_stop_xy(stop)[0];
-                                })
-                                .attr('cy', function (stop) {
-                                    return get_stop_xy(stop)[1];
-                                }); 
-
-                            circles.exit()
-                                .remove();
+                            .attr('cx', function (stop) {
+                                return get_stop_xy(stop)[0];
+                            })
+                            .attr('cy', function (stop) {
+                                return get_stop_xy(stop)[1];
+                            }); 
                         }
                     };
 
-                    window.setInterval(function () {
+                    this.interval_id = window.setInterval(function () {
                         get_locs(update_vehicles);
                     }, 10000);
-                // }
+                }
+
+            });
+
+
+            var route_view_map = d3.map();
+            _.each(routes, function (route) {
+
+                // create controls
+                var render_route_buttons = function () {
+                    var active = route_ui_state.get(route.tag.replace(' ', '-')) ? ' active':'';
+                    $('#controls').append('<button type="button" class="btn toggle-route-button'+active+'" data-route-tag='+route.tag+'>'+route.tag+'</button>');
+                }();
+
+                var rv = new RouteView(route);
+                rv.show(route_ui_state.get(route.tag));
+                route_view_map.set(route.tag, rv);
+            });
+
+            $('.toggle-route-button').click(function () {
+                var route_tag = $(this).data('routeTag');
+                console.log('route_tag:',route_tag);
+                var toggle = function(map, key) {
+                    map.set(key, !map.get(key));
+                };
+                toggle(route_ui_state, route_tag);
+                route_view_map.get(route_tag).show(route_ui_state.get(route_tag));
+                $(this).toggleClass('active');  
             });
 
         });
